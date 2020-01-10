661ad9b15bc27a26af6b0aa975b401e8a01b32ac
issam <issamyayou@gmail.com>
2020-01-10 15:06:16 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+	function setColorOfToday() {
+		   var days = document.getElementsById('day');
+		   for (var i = days.length - 1; i >= 0; i--) {
+		   		if (days[i]. == date.getDayName()) {
+		   		   days[i].style.backgroundColor = '#502314' //get the name od day from th tag 
+
+		   		}
+
+		   	}	
+
+		}
